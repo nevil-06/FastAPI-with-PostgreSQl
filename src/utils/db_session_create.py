684aafad0699ba00a9b9e676fd50d76427d82ea7
Nevil Italiya <nevilitaliya00@gmail.com>
2023-01-29 11:11:30 +0530

    Removed all files

--- src/utils/db_session_create.py
+++
@@ -1,8 +0,0 @@
-from src.database_files.database import SessionLocal
-
-def get_db():
-    try:
-        db = SessionLocal()
-        return db
-    except AttributeError:
-        print("Can not get the DB.")
--- src/utils/update.py
+++
@@ -1,5 +0,0 @@
-
-# def update_fields(self, update: dict):
-#         for key, value in update.items():
-#             if value:
-#                 setattr(self, key, value)
